8e7da4960385d8ce491b86744ccaeefc4b44c089
ajr250571 <ajr250571@gmail.com>
2024-05-15 15:32:02 -0300

    First Commit

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,13 @@
+import { BookmarkIcon } from "@radix-ui/react-icons";
+
+export default function RegisterPage() {
+  return (
+    <div className="flex flex-col items-center content-center h-screen gap-2">
+      RegisterPage
+      <button className="btn btn-primary">
+        <BookmarkIcon />
+        Bookmark
+      </button>
+    </div>
+  );
+}
