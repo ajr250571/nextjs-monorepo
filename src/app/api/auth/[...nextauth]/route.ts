5e0fc34d4998b474fe323a19f3ec8270addcbf89
ajr250571 <ajr250571@gmail.com>
2024-05-20 16:25:32 -0300

    project user relations

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,9 @@
 import bcrypt from "bcrypt";
 import prisma from "@/libs/prisma";
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler = NextAuth({
+export const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -35,9 +35,32 @@ const handler = NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+    async session({ session, user, token }) {
+      if (token) {
+        session.user.id = token.sub as string;
+      }
+
+      return session;
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
   pages: {
     signIn: "/auth/login",
   },
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -1,11 +1,27 @@
 import prisma from "@/libs/prisma";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function POST(request: NextRequest) {
   const data = await request.json();
-  console.log(data);
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json(
+      { message: "Session Unauthorized." },
+      { status: 401 }
+    );
+  }
   const newProject = await prisma.project.create({
-    data,
+    data: {
+      title: data.title,
+      description: data.description,
+      user: {
+        connect: {
+          id: Number(session.user.id),
+        },
+      },
+    },
   });
   return NextResponse.json(newProject, { status: 201 });
 }
